474285036b321b4bf1ddc6e943de7718134ba2bd
S.S <tectest58@gmail.com>
2018-06-18 06:34:14 +0000

    user login

--- resources/views/commons/error_messages.blade.php
+++ resources/views/commons/error_messages.blade.php
@@ -1,8 +1,5 @@
-@if(count($errors) > 0)
-<ul>
-    @foreach($errors->all() as $error)
-        <div class="alert alert-warning">{{$error}}</div>
+@if (count($errors) > 0)
+    @foreach ($errors->all() as $error)
+        <div class="alert alert-warning">{{ $error }}</div>
     @endforeach
-</ul>
 @endif
-
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,14 +12,13 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
-
     <body>
         @include('commons.navbar')
 
         <div class="container">
             @include('commons.error_messages')
-            @yield('content')
-        </div><!--/container-->
-    </body>
 
+            @yield('content')
+        </div>
+    </body>
 </html>
